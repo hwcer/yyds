c36324c4293b8574edad8d372a5abf0979bc0154
万超 黄 <hwclegend@163.com>
2025-05-23 22:01:53 +0800

    update

--- players/player/times.go
+++ players/player/times.go
@@ -7,15 +7,11 @@ import (
 	"github.com/hwcer/yyds/options"
 )
 
+// 更多参考 times.ExpireType
+
 const (
-	ExpireTimeNone         int32 = 0
-	ExpireTimeDaily        int32 = 1
-	ExpireTimeWeekly       int32 = 2
-	ExpireTimeMonthly      int32 = 3
-	ExpireTimeTimeStamp    int32 = 4
-	ExpireTimePlayerCreate int32 = 5
-	ExpireTimeServerCreate int32 = 6
-	ExpireTimeServerAlways int32 = 9 //终身
+	ExpireTimePlayerCreate times.ExpireType = 8
+	ExpireTimeServerCreate times.ExpireType = 9
 )
 
 type Times struct {
@@ -23,20 +19,29 @@ type Times struct {
 }
 
 // Start 开始时间
-func (this *Times) Start(t int32, v int64) (r int64, err error) {
-	if v < 1 {
+func (this *Times) Start(t int64, v int64) (r int64, err error) {
+	if v == 0 {
 		v = 1
 	}
-	switch t {
-	case ExpireTimeNone:
+	et := times.ExpireType(t)
+	switch et {
+	case times.ExpireTypeNone:
 		return
-	case ExpireTimeDaily:
+	case times.ExpireTypeDaily:
 		return times.Daily(0).Now().Unix(), nil
-	case ExpireTimeWeekly:
+	case times.ExpireTypeWeekly:
 		return times.Weekly(0).Now().Unix(), nil
-	case ExpireTimeMonthly:
+	case times.ExpireTypeMonthly:
 		return times.Monthly(0).Now().Unix(), nil
-	case ExpireTimeTimeStamp:
+	case times.ExpireTypeSecond:
+		return v, nil
+	case times.ExpireTypeCustomize:
+		var ttl *times.Times
+		if ttl, err = times.ParseExpireTypeCustomize(int(v)); err == nil {
+			r = ttl.Now().Unix()
+		}
+		return
+	case times.ExpireTimeTimeStamp:
 		return v, nil
 	case ExpireTimePlayerCreate:
 		role := this.p.Document(ITypeRole)
@@ -47,8 +52,6 @@ func (this *Times) Start(t int32, v int64) (r int64, err error) {
 		dt := times.Unix(options.GetServerTime())
 		dt = dt.Daily(int(v - 1))
 		return dt.Now().Unix(), nil
-	case ExpireTimeServerAlways:
-		return 1, nil
 	default:
 		err = values.Errorf(0, "time type unknown")
 		return
@@ -62,25 +65,27 @@ func (this *Times) ExpireWithArray(expire ...int64) (r int64, err error) {
 	if len(expire) < 2 {
 		expire = append(expire, 1)
 	}
-	return this.Expire(int32(expire[0]), expire[1])
+	return this.Expire(expire[0], expire[1])
 }
 
 // Expire 过期时间
-func (this *Times) Expire(t int32, v int64) (r int64, err error) {
-	switch t {
-	case ExpireTimeNone:
-		return
-	case ExpireTimeDaily, ExpireTimeWeekly, ExpireTimeMonthly:
+func (this *Times) Expire(t int64, v int64) (r int64, err error) {
+	if t == 0 {
+		return 0, nil
+	}
+	if v == 0 {
+		v = 1
+	}
+	et := times.ExpireType(t)
+	if et.Has() {
 		var ts *times.Times
-		if v == 0 {
-			v = 1
-		}
-		if ts, err = times.Expire(times.ExpireType(t), int(v)); err == nil {
+		if ts, err = times.Expire(et, int(v)); err == nil {
 			r = ts.Now().Unix()
 		}
 		return
-	case ExpireTimeTimeStamp:
-		return v, nil
+	}
+
+	switch et {
 	case ExpireTimePlayerCreate:
 		if v > 0 {
 			role := this.p.Document(ITypeRole)
@@ -96,8 +101,6 @@ func (this *Times) Expire(t int32, v int64) (r int64, err error) {
 			r = dt.Now().Unix()
 		}
 		return
-	case ExpireTimeServerAlways:
-		return times.Now().Unix() + times.DaySecond*365*100, nil
 	default:
 		err = values.Errorf(0, "time type unknown")
 		return
@@ -106,15 +109,16 @@ func (this *Times) Expire(t int32, v int64) (r int64, err error) {
 
 // Verify 验证是否在有效期(开始以及过期时间)内，返回开始和结束时间
 func (this *Times) Verify(args []int64) (t [2]int64, err error) {
-	args = append(args, 0, 0, 0)
+	arr := append([]int64{}, args...)
+	arr = append(arr, 0, 0, 0)
 	now := times.Now().Unix()
-	if t[0], err = this.Start(int32(args[0]), args[1]); err != nil {
+	if t[0], err = this.Start(arr[0], arr[1]); err != nil {
 		return
 	} else if t[0] > now {
 		err = errors.ErrActiveDisable
 		return
 	}
-	if t[1], err = this.Expire(int32(args[0]), args[2]); err != nil {
+	if t[1], err = this.Expire(arr[0], arr[2]); err != nil {
 		return
 	}
 	if t[1] > 0 && t[1] < now {
