db8d1b53e4d5ac9a1294eddc5f818c020b0de4d7
万超 黄 <hwclegend@163.com>
2025-08-29 14:59:06 +0800

    update

--- gateway/players/players.go
+++ gateway/players/players.go
@@ -1,12 +1,12 @@
 package players
 
 import (
-	"fmt"
+	"strings"
+	"sync"
+
 	"github.com/hwcer/cosgo/session"
 	"github.com/hwcer/cosgo/values"
 	"github.com/hwcer/cosnet"
-	"strings"
-	"sync"
 )
 
 const (
@@ -88,50 +88,5 @@ func (this *players) Login(guid string, value values.Values, callback loginCallb
 	if callback != nil {
 		err = callback(r, loaded)
 	}
-	return
-}
-
-func (this *players) Connect(sock *cosnet.Socket, guid string, value values.Values) error {
-	err := this.Login(guid, value, func(data *session.Data, loaded bool) error {
-		if loaded {
-			this.replace(data, sock)
-		}
-		data.Set(SessionPlayerSocketName, sock, true)
-		sock.OAuth(data)
-		return nil
-	})
-	return err
-}
-
-func (this *players) Disconnect(sock *cosnet.Socket) (err error) {
-	i := sock.Data()
-	if i == nil {
-		return
-	}
-	data, ok := i.(*session.Data)
-	if !ok {
-		return fmt.Errorf("socket data type error:%v", i)
-	}
-
-	data.Lock()
-	defer data.Unlock()
-	if s := this.Socket(data); s != nil && s.Id() == sock.Id() {
-		data.Delete(SessionPlayerSocketName, true)
-	}
-	return
-}
-
-func (this *players) Reconnect(sock *cosnet.Socket, secret string) (data *session.Data, err error) {
-	if v := sock.Data(); v != nil {
-		return
-	}
-	s := session.New()
-	if err = s.Verify(secret); err != nil {
-		return
-	}
-	data = s.Data
-	this.replace(data, sock)
-	data.Set(SessionPlayerSocketName, sock)
-	sock.Reconnect(data)
 	return
 }
